abe37478801044898a635a7d36f466fc13eef299
BuhleDamane <ziziphodamane@gmail.com>
2024-08-16 03:04:59 +0200

    Updated app.js & weather.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Weather from "./Weather.js"
 function App() {
   return (
     <div className="App">
-    <Weather />
+    <Weather defaultCity="Carletonville"/>
     <footer>
             This Project was coded by Buhle Damane and is open-sourced on Github.
         </footer>
--- src/Weather.js
+++ src/Weather.js
@@ -4,33 +4,44 @@ import './Weather.css'
 import Search from "./Search"
 
 
-export default function Weather(){
-    const [ready, setReady] = useState(false);
-    const [temperature, setTemperature] = useState (null);
+export default function Weather(props){
+    const [weatherData, setWeatherData] = useState ({ready: false});
     function handleResponse(response){
         console.log(response.data)
-        setTemperature(response.data.main.temp);
-        setReady(true);
+
+
+        setWeatherData({
+            ready: true,
+            temperature: response.data.main.temp,
+            humidity: response.data.main.humidity,
+            data: "Wednesday 07:00",
+            description: response.data.weather[0].description,
+            iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+            wind: response.data.wind.temp,
+            city: response.data.name
+
+        });
     }
 
-    if(ready){
+    if(weatherData.ready){
        return(
             <div className="Weather">
                 <Search />
-            <h1>Carletonville</h1>
+            <h1>{weatherData.city}</h1>
             <ul className="lol">
                 <li>
-                    Wednesday 07:00
+                    {weatherData.date}
                 </li>
-                <li>Moslty Cloudy</li>
+                <li className="text-capitalize">{weatherData.description}</li>
             </ul>
            <div className="row">
             <div className="col-6">
                 <div className="clearFix">
-            <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" className="float-left" alt="Partly Cloudy" />
+            <img src={weatherData.iconUrl} 
+            className="float-left" alt={weatherData.description} />
             </div>
             <div className="float-left">
-                <span className="temperature">{temperature}</span><span className="unit">°C</span>|<span>°F</span>
+                <span className="temperature">{Math.round(weatherData.temperature)}</span><span className="unit">°C</span>|<span>°F</span>
                 </div>
             </div>
             <div className="col-6">
@@ -39,10 +50,10 @@ export default function Weather(){
                     Precipitation: 15%
                 </li>
                 <li>
-                    Humidity: 72%
+                    Humidity: {weatherData.humidity}%
                 </li>
                 <li>
-                    Wind: 13 km/h
+                   wind: {weatherData.wind} 13 km/h
                 </li>
             </ul>
             </div>
@@ -51,11 +62,10 @@ export default function Weather(){
         );
 
     } else{
-        const apiKey = "13d2ee51f9b4o75695t30ad14de90637";
-        let city = "Carletonville";
-        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+        const apiKey = "9cb72bec958f8fb02391985ed7b219d2";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
         axios.get(apiUrl).then(handleResponse);
-       return "iyeza....";
+       return "Loading....";
     }
     
 }
